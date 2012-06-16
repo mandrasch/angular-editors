1fa64033d04787ca6baa5bdc9ec9aa2e93764359
Matthias <johnnyclash23@gmail.com>
2012-06-16 21:41:11 +0200

    - some improvements, thanks to ajoslin (https://gist.github.com/2942067)

--- editors/pagedown-bootstrap/directive.js
+++ editors/pagedown-bootstrap/directive.js
@@ -3,6 +3,10 @@
  */
 myApp.directive('uiPagedownBootstrap', function($compile) {
     var nextId = 0;
+    
+    //Make converter only once to save a bit of load each time - thanks to ajoslin
+    var markdownConverter = new Markdown.Converter(); 
+    
     return {
         require: 'ngModel',
         replace:true,
@@ -14,7 +18,7 @@ myApp.directive('uiPagedownBootstrap', function($compile) {
             var newElement = $compile('<div>'+
                 '<div class="wmd-panel">'+
                 '<div id="wmd-button-bar-'+editorUniqueId+'"></div>'+
-                '<textarea class="wmd-input" id="wmd-input-'+editorUniqueId+'">{{modelValue()}}'+
+                '<textarea class="wmd-input" id="wmd-input-'+editorUniqueId+'">'+
                 '</textarea>'+
                 '</div>'+
                 '<div id="wmd-preview-'+editorUniqueId+'" class="wmd-panel wmd-preview"></div>'+
@@ -34,28 +38,35 @@ myApp.directive('uiPagedownBootstrap', function($compile) {
             });
             
             editor.run();
+            
+            var $wmdInput = $("#wmd-input-"+editorUniqueId);
                     
                     
             // local -> parent scope change (model)
-            $("#wmd-input-"+editorUniqueId).on('change',function(){
-                console.log('change - raw content: ',$(this).val());
-                var rawContent = $(this).val();
-                ngModel.$setViewValue(rawContent);
-                scope.$apply();
+            $wmdInput.on('change',function(){
+                console.log('wmdInput changed',$wmdInput.val());
+                var rawContent = $wmdInput.val();
+                scope.$apply(function(){
+                    ngModel.$setViewValue(rawContent);
+                });
             });
             
-            
-           
             // parent scope -> local change
-            scope.modelValue = function() {
-                return ngModel.$viewValue;
-            };
-            
-            
-        // 2DO: do we have to do this?
-        // we watch the view value, otherwise the editor is not updated
-        // editor.refreshPreview() forces the editor to re-render the preview according to the contents of the input, e.g. after you have programmatically changed the input box content. This method is only available after editor.run() has been called.
-            
+            scope.$watch(iAttrs.ngModel, function(value,oldValue) {
+                console.log('ngModel changed','old: ',oldValue,'new: ',value,editor,$wmdInput);
+                // this does not really work, so we do it manually - what is the correct way?
+                // scope.textareaValue = value;
+                if(value!==undefined){
+                    /*scope.$apply(function(){
+                        textareaValue = value;
+                        editor.refreshPreview();
+                    })*/
+                    $wmdInput.val(value);              
+                    //console.log($wmdInput.html());
+                    editor.refreshPreview(); // forces the editor to re-render the preview according to the contents of the input, e.g. after you have programmatically changed the input box content. This method is only available after editor.run() has been called.
+                }
+               
+            });            
         }
     }
 });
