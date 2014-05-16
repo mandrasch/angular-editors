13eff268deb091046f6514807bf4a5f480d08fba
Porjo <porjo38@yahoo.com.au>
2014-05-16 15:32:55 +1000

    Fix binding

--- editors/pagedown-bootstrap/directive.js
+++ editors/pagedown-bootstrap/directive.js
@@ -1,24 +1,26 @@
 /*
  *usage: <textarea ui:pagedown-bootstrap ng:model="box.content"></textarea>
  */
-myApp.directive('uiPagedownBootstrap', function($compile) {
+myApp.directive('uiPagedownBootstrap',['$compile', function($compile) {
     var nextId = 0;
     
     //Make converter only once to save a bit of load each time - thanks to ajoslin
-    var markdownConverter = new Markdown.Converter(); 
-    
+    var converter = new Markdown.Converter();
+
     return {
-        require: 'ngModel',
         replace:true,
+        scope: {
+            markdown: '='
+        },
         template:'<div class="pagedown-bootstrap-editor"></div>',
-        link:function (scope, iElement, iAttrs, ngModel) {
+        link:function (scope, iElement, iAttrs) {
                     
             var editorUniqueId = nextId++;
             
             var newElement = $compile('<div>'+
                 '<div class="wmd-panel">'+
                 '<div id="wmd-button-bar-'+editorUniqueId+'"></div>'+
-                '<textarea class="wmd-input" id="wmd-input-'+editorUniqueId+'">'+
+                '<textarea class="wmd-input" id="wmd-input-'+editorUniqueId+'" ng-model="markdown">'+
                 '</textarea>'+
                 '</div>'+
                 '<div id="wmd-preview-'+editorUniqueId+'" class="wmd-panel wmd-preview"></div>'+
@@ -26,47 +28,33 @@ myApp.directive('uiPagedownBootstrap', function($compile) {
             
             iElement.html(newElement);
             
-            var converter = new Markdown.Converter();
-            
             var help = function () {
                 // 2DO: add nice modal dialog
                 alert("Do you need help?");
-            }
-            
+            };
+
+            converter.hooks.chain("preConversion", function (text) {
+                //console.log("preconversion", text);
+                if(text !== "" && scope.markdown !== text) {
+                    scope.markdown = text;
+                }
+                return text;
+            });
+
             var editor = new Markdown.Editor(converter, "-"+editorUniqueId, {
                 handler: help
             });
             
             editor.run();
-            
-            var $wmdInput = $("#wmd-input-"+editorUniqueId);
-                    
-                    
-            // local -> parent scope change (model)
-            $wmdInput.on('change',function(){
-                console.log('wmdInput changed',$wmdInput.val());
-                var rawContent = $wmdInput.val();
-                scope.$apply(function(){
-                    ngModel.$setViewValue(rawContent);
-                });
-            });
-            
+
             // parent scope -> local change
-            scope.$watch(iAttrs.ngModel, function(value,oldValue) {
-                console.log('ngModel changed','old: ',oldValue,'new: ',value,editor,$wmdInput);
-                // this does not really work, so we do it manually - what is the correct way?
-                // scope.textareaValue = value;
-                if(value!==undefined){
-                    /*scope.$apply(function(){
-                        textareaValue = value;
-                        editor.refreshPreview();
-                    })*/
-                    $wmdInput.val(value);              
-                    //console.log($wmdInput.html());
+            scope.$watch("markdown", function(value,oldValue) {
+                //console.log('ngModel changed','old: ',oldValue,'new: ',value);
+                if(angular.isDefined(value)){
                     editor.refreshPreview(); // forces the editor to re-render the preview according to the contents of the input, e.g. after you have programmatically changed the input box content. This method is only available after editor.run() has been called.
                 }
                
-            });            
+            });
         }
-    }
-});
+    };
+}]);
